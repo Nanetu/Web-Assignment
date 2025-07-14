40420bc2d26b741aef347ac4769866f013d7ff31
Nanetu <nanetumushongoma@gmail.com>
2025-07-14 22:31:00 +0200

    Fixed sql query

--- api/memes/user_memes.php
+++ api/memes/user_memes.php
@@ -6,13 +6,10 @@ require_once __DIR__.'/../../utils/database.php';
 
 header('Content-Type: application/json');
 
-if(!$_SERVER['uid']){
+if (!isset($_SESSION['uid'])) {
     http_response_code(401);
-    echo json_encode([
-        'success'=>false,
-        'message'=>'Authentication required'
-    ]);
-    return;
+    echo json_encode(['success' => false, 'message' => 'Authentication required']);
+    exit;
 }
 
 if($_SERVER['REQUEST_METHOD'] !== 'POST'){
@@ -27,18 +24,19 @@ if($_SERVER['REQUEST_METHOD'] !== 'POST'){
 $user = $_SESSION['uid'];
 
 $sql = "SELECT
-         m.*,
-        SUM(r.type = 'Like') as like_count,
-        SUM(r.type = 'Upvote') as upvote_count,
-        SUM(r.type = 'Share'), as share_countc,
-        SUM(r.type = 'Download') as download_count
+            m.*,
+            SUM(r.type = 'Like') AS like_count,
+            SUM(r.type = 'Upvote') AS upvote_count,
+            SUM(r.type = 'Share'), AS share_countc,
+            SUM(r.type = 'Download') AS download_count
         FROM 
-        memes m
+            memes m
         LEFT JOIN 
-        reactions r on r.meme_id = m.meme_id
+            reactions r on m.meme_id = r.meme_id
+        WHERE 
+            m.meme_id = ?
         GROUP BY
-        m.meme_id
-        HAVING m.meme_id = ?
+            m.meme_id
 ";
 
 try{
